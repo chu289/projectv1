86c46db0d22c474f44bd15addcafa1ae89a1c0fb
chu289 <asd0903a@gmail.com>
2021-12-08 00:41:47 +0800

    1208init

---
+++ src/views/Home.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="home">
+    <div class="wrap">
+      <div class="header">
+        <div class="header__menu">
+          <img src="@/assets/menu.png" alt="" />
+        </div>
+        <div class="header__title">
+          <h3>醫療好評網</h3>
+        </div>
+        <div class="header__search">
+          <div class="sub1">O</div>
+          <div class="sub1__line"></div>
+          <div class="sub2">PEN</div>
+        </div>
+        <a class="scroll__btn" href="#subtitle">
+          <img src="@/assets/nextPage.png" alt="" />
+        </a>
+        <div class="header__subtitle" id="subtitle">
+          <h3>醫療好評網</h3>
+          <h4>給您的安心有保障的醫療評價資訊， <br />簡單明瞭、方便操作 。</h4>
+        </div>
+      </div>
+    </div>
+    <div class="content1">
+      <div class="content1__title">
+        <h3>你有這些困擾嗎?</h3>
+      </div>
+      <div class="content1__box">
+        <div class="content1__box__img">
+          <img src="@/assets/thunder.png" alt="" />
+        </div>
+        <h3>臨時找醫院，妳怕雷嗎?</h3>
+      </div>
+      <div class="content1__box">
+        <div class="content1__box__img">
+          <img src="@/assets/talk.png" alt="" />
+        </div>
+        <h3>道聽塗說是不是你求醫的唯一參考?</h3>
+      </div>
+      <div class="content1__box">
+        <div class="content1__box__img">
+          <img src="@/assets/time.png" alt="" />
+        </div>
+        <h3>怕花太多時間尋找妳心中的良醫嗎?</h3>
+      </div>
+      <div class="content1__box">
+        <div class="content1__box__img">
+          <img src="@/assets/layout.png" alt="" />
+        </div>
+        <h3>版面太雜，使用起來霧煞煞嗎？</h3>
+      </div>
+      <div class="content1__subbox">
+        <div class="content1__box__img">
+          <img src="@/assets/trust.png" alt="" />
+        </div>
+        <h3>
+          面對意外，<br />你需要的是快速精準的醫療資訊，<br />
+          而不是片面之詞的傳言，<br />讓我們為你找到值得信賴的醫療機構。
+        </h3>
+      </div>
+    </div>
+    <div class="content2">
+      <img src="@/assets/Group13.png" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style type="text/css" src="@/assets/style.css"></style>
